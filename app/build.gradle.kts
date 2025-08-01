aa7f10dd4b05b248d499a179771dd48402a7c89a
Kamshanski <darkuszhenya@gmail.com>
2025-08-02 03:38:51 +0700

    Chrome plugin module

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.dsl.JsSourceMapEmbedMode
+import org.jetbrains.kotlin.gradle.dsl.JsSourceMapNamesPolicy
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+
+plugins {
+	alias(libs.plugins.kotlinMultiplatform)
+	alias(libs.plugins.kotlinxSerialization)
+}
+
+kotlin {
+	js {
+		binaries.executable()
+		browser {
+			commonWebpackConfig {
+				cssSupport {
+					enabled.set(true)
+				}
+			}
+			testTask {
+				testLogging.showStandardStreams = true
+				useKarma {
+					useChromeHeadless()
+				}
+			}
+		}
+	}
+
+	sourceSets {
+		jsMain.dependencies {
+			implementation(libs.kotlinxSerialization.core)
+			implementation(libs.kotlinxSerialization.json)
+		}
+		jsTest.dependencies {
+			implementation(libs.kotlin.test)
+		}
+	}
+}
+
+tasks.withType<Kotlin2JsCompile>().configureEach {
+	compilerOptions.sourceMapNamesPolicy.set(JsSourceMapNamesPolicy.SOURCE_MAP_NAMES_POLICY_FQ_NAMES)
+	compilerOptions.sourceMapEmbedSources.set(JsSourceMapEmbedMode.SOURCE_MAP_SOURCE_CONTENT_ALWAYS)
+}
---
+++ app/src/jsMain/kotlin/dev/kamshanski/chromeplugintemplate/Main.kt
@@ -0,0 +1,9 @@
+package dev.kamshanski.chromeplugintemplate
+
+import kotlinx.browser.window
+
+fun main() {
+    window.onload = {
+
+    }
+}
