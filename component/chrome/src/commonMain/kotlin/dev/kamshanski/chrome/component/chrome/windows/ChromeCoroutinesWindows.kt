797aca7c53676f74a9a167d066c07aed2c7aebca
Kamshanski <darkuszhenya@gmail.com>
2025-08-09 22:22:17 +0700

    Added ChromeCoroutinesWindows

--- component/chrome/src/commonMain/kotlin/dev/kamshanski/chrome/component/chrome/windows/ChromeCoroutinesWindows.kt
+++ component/chrome/src/commonMain/kotlin/dev/kamshanski/chrome/component/chrome/windows/ChromeCoroutinesWindows.kt
@@ -1,8 +1,13 @@
-@file:JsQualifier("chrome")
-
-package chrome.windows
+package dev.kamshanski.chrome.component.chrome.windows
 
 import chrome.events.Event
+import chrome.windows.ChromeWindows
+import chrome.windows.CreateData
+import chrome.windows.QueryOptions
+import chrome.windows.UpdateInfo
+import chrome.windows.Window
+import chrome.windows.WindowsEvent
+import kotlinx.coroutines.await
 import kotlin.js.Promise
 import kotlin.js.collections.JsReadonlyArray
 
@@ -11,89 +16,81 @@ import kotlin.js.collections.JsReadonlyArray
  *
  * Permissions: The chrome.windows API can be used without declaring any permission. However, the "tabs" permission is required in order to populate the url, title, and favIconUrl properties of Tab objects.
  */
-@JsName("windows")
 @Suppress("unused")
-external object ChromeWindows {
+object ChromeCoroutinesWindows {
 
 	/** The windowId value that represents the current window. */
-	val WINDOW_ID_CURRENT: Int /* -2 */
+	val WINDOW_ID_CURRENT: Int /* -2 */ by ChromeWindows::WINDOW_ID_CURRENT
 
 	/** The windowId value that represents the absence of a Chrome browser window */
-	val WINDOW_ID_NONE: Int /* -1 */
+	val WINDOW_ID_NONE: Int /* -1 */ by ChromeWindows::WINDOW_ID_NONE
 
 	/**
 	 * Gets details about a window.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun get(windowId: Int, queryOptions: QueryOptions = definedExternally): Promise<Window>
-	fun get(windowId: Int, callback: (window: Window) -> Unit)
-	fun get(windowId: Int, queryOptions: QueryOptions?, callback: (window: Window) -> Unit)
+	suspend fun get(windowId: Int): Window = ChromeWindows.get(windowId).await()
+	suspend fun get(windowId: Int, queryOptions: QueryOptions): Window = ChromeWindows.get(windowId, queryOptions).await()
 
 	/**
 	 * Gets the current window.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun getCurrent(queryOptions: QueryOptions = definedExternally): Promise<Window>
-	fun getCurrent(callback: (window: Window) -> Unit)
-	fun getCurrent(queryOptions: QueryOptions?, callback: (window: Window) -> Unit)
+	suspend fun getCurrent(): Window = ChromeWindows.getCurrent().await()
+	suspend fun getCurrent(queryOptions: QueryOptions): Window = ChromeWindows.getCurrent(queryOptions).await()
 
 	/**
 	 * Creates (opens) a new browser window with any optional sizing, position, or default URL provided.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun create(createData: CreateData = definedExternally): Promise<Window?>
-	fun create(callback: (window: Window? /* use undefined for default */) -> Unit)
-	fun create(createData: CreateData?, callback: (window: Window? /* use undefined for default */) -> Unit)
+	suspend fun create(): Window? = ChromeWindows.create().await()
+	suspend fun create(createData: CreateData): Window? = ChromeWindows.create(createData).await()
 
 	/**
 	 * Gets all windows.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun getAll(queryOptions: QueryOptions = definedExternally): Promise<JsReadonlyArray<Window>>
-	fun getAll(callback: (windows: JsReadonlyArray<Window>) -> Unit)
-	fun getAll(queryOptions: QueryOptions?, callback: (windows: JsReadonlyArray<Window>) -> Unit)
+	suspend fun getAll(): JsReadonlyArray<Window> = ChromeWindows.getAll().await()
+	suspend fun getAll(queryOptions: QueryOptions): JsReadonlyArray<Window> = ChromeWindows.getAll(queryOptions).await()
 
 	/**
 	 * Updates the properties of a window. Specify only the properties that to be changed; unspecified properties are unchanged.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun update(windowId: Int, updateInfo: UpdateInfo): Promise<Window>
-	fun update(windowId: Int, updateInfo: UpdateInfo, callback: (window: Window) -> Unit)
+	suspend fun update(windowId: Int, updateInfo: UpdateInfo): Window = ChromeWindows.update(windowId, updateInfo).await()
 
 	/**
 	 * Removes (closes) a window and all the tabs inside it.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun remove(windowId: Int): Promise<Unit>
-	fun remove(windowId: Int, callback: () -> Unit)
+	suspend fun remove(windowId: Int) = ChromeWindows.remove(windowId).await()
 
 	/**
 	 * Gets the window that was most recently focused — typically the window 'on top'.
 	 *
 	 * Can return its result via Promise in Manifest V3 or later since Chrome 88.
 	 */
-	fun getLastFocused(queryOptions: QueryOptions = definedExternally): Promise<Window>
-	fun getLastFocused(callback: (window: Window) -> Unit)
-	fun getLastFocused(queryOptions: QueryOptions?, callback: (window: Window) -> Unit)
+	suspend fun getLastFocused(): Window  = ChromeWindows.getLastFocused().await()
+	suspend fun getLastFocused(queryOptions: QueryOptions): Window  = ChromeWindows.getLastFocused(queryOptions).await()
 
 	/** Fired when a window is removed (closed). */
-	val onRemoved: WindowsEvent<(windowId: Int) -> Unit>
+	val onRemoved: WindowsEvent<(windowId: Int) -> Unit> by ChromeWindows::onRemoved
 
 	/** Fired when a window is created. */
-	val onCreated: WindowsEvent<(window: Window) -> Unit>
+	val onCreated: WindowsEvent<(window: Window) -> Unit> by ChromeWindows::onCreated
 
 	/** Fired when the currently focused window changes. Returns `chrome.windows.WINDOW_ID_NONE` if all Chrome windows have lost focus. **Note:** On some Linux window managers, `WINDOW_ID_NONE` is always sent immediately preceding a switch from one Chrome window to another. */
-	val onFocusChanged: WindowsEvent<(windowId: Int) -> Unit>
+	val onFocusChanged: WindowsEvent<(windowId: Int) -> Unit> by ChromeWindows::onFocusChanged
 
 	/**
 	 * Fired when a window has been resized; this event is only dispatched when the new bounds are committed, and not for in-progress changes.
 	 * @since Chrome 86
 	 */
-	val onBoundsChanged: Event<(window: Window) -> Unit>
+	val onBoundsChanged: Event<(window: Window) -> Unit> by ChromeWindows::onBoundsChanged
 }
